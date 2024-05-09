c29882c81e86e904cb0573e0fad23a42169d34ed
Stiven Gomez <stivenalexander7u7@gmail.com>
2024-05-08 19:23:29 -0500

    FIX - Ajuste de detalle de la receta.

--- src/app/components/details-recipe/details-recipe.component.ts
+++ src/app/components/details-recipe/details-recipe.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GetRecipeService } from '../../core/services/get-recipe.service';
+import { QueryParams, Recipe, RecipeDetail } from '../../core/models';
 
 @Component({
   selector: 'app-details-recipe',
@@ -8,5 +11,36 @@ import { Component } from '@angular/core';
   styleUrl: './details-recipe.component.scss'
 })
 export class DetailsRecipeComponent {
+
+  recipeResults: RecipeDetail[] = [];
+
+  constructor(private getRecipeService: GetRecipeService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    var params = this.route.snapshot.params as QueryParams;
+
+    this.getRecipeService.getRecipeById(params.id).subscribe((data) => {
+      console.log(data);
+
+      var tagsFormat: Array<String> = [];
+
+      if (data.meals[0].strTags != "" && data.meals[0].strTags != null) {
+        var tags = data.meals[0].strTags.split(',')
+        for (let i = 0; i < tags.length; i++) {
+          tagsFormat.push(tags[i]);
+        }
+      }
+
+      this.recipeResults = data.meals;
+      this.recipeResults[0].tagsFormat = tagsFormat;
+
+    });
+
+  }
+
+  viewMore(url: string) {
+    var newUrl = url;
+    window.open(newUrl, "_blank");
+  }
 
 }
